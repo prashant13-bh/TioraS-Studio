ef9ec4bba5da768e82edd954ec30fdf8e55f03e5
Prashant Hiremath <yuvasamrajyaofficial@gmail.com>
2025-12-28 13:30:19 +0530

    Update Firebase config and add google-services.json

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-8710289248-529e5",
-  "appId": "1:372408711772:web:aebaf1aeaf89529cd2c499",
-  "apiKey": "AIzaSyAYeahHju7x8iaaddsNQMeHFor36iP_JDg",
-  "authDomain": "studio-8710289248-529e5.firebaseapp.com",
-  "measurementId": "G-7KG3YYFH21",
-  "messagingSenderId": "372408711772"
+  apiKey: "AIzaSyAYeahHju7x8iaaddsNQMeHFor36iP_JDg",
+  authDomain: "studio-8710289248-529e5.firebaseapp.com",
+  databaseURL: "https://studio-8710289248-529e5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-8710289248-529e5",
+  storageBucket: "studio-8710289248-529e5.firebasestorage.app",
+  messagingSenderId: "372408711772",
+  appId: "1:372408711772:web:aebaf1aeaf89529cd2c499",
+  measurementId: "G-7KG3YYFH21"
 };
